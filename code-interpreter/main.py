8d56451d169f69316d4102fa1dce8d5832c1e669
Erick Cardenas <erick.mendez@toptal.com>
2024-02-14 16:07:17 -0500

    end of chapter 38

--- code-interpreter/main.py
+++ code-interpreter/main.py
@@ -1,12 +1,16 @@
 """
 I deviated a lot from the example given in the course.
-Instead, I followed the example provided in the LangChain docs:
-https://python.langchain.com/docs/integrations/toolkits/python
+
+Instead, I followed the examples provided in the LangChain docs:
+- https://python.langchain.com/docs/integrations/toolkits/python
+- https://python.langchain.com/docs/integrations/toolkits/csv
 """
 
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import AgentExecutor, create_openai_functions_agent
+from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain_experimental.tools import PythonREPLTool
 from langchain_openai import ChatOpenAI
 
@@ -32,11 +36,27 @@ def main():
     python_agent = create_openai_functions_agent(llm, tools, prompt)
     python_agent_executor = AgentExecutor(agent=python_agent, tools=tools, verbose=True)
 
-    python_agent_executor.invoke(
+    #     python_agent_executor.invoke(
+    #         {
+    #             "input": """Generate and save in the current working directory 15 QR codes
+    # that point to www.udemy.com/course/langchain. You have the qrcode package installed already.
+    # """
+    #         }
+    #     )
+
+    csv_agent_executor = create_csv_agent(
+        llm=llm,
+        path="episode_info.csv",
+        verbose=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+    )
+
+    csv_agent_executor.invoke(
+        {"input": "how many columns are there in file episode_info.csv"}
+    )
+    csv_agent_executor.invoke(
         {
-            "input": """Generate and save in the current working directory 15 QR codes
-that point to www.udemy.com/course/langchain. You have the qrcode package installed already.
-"""
+            "input": "print the seasons in ascending order of the number of episodes they have"
         }
     )
 
